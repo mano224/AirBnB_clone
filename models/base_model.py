ea0aeeb2dec349eabaffde3e0a08aeb84ab46870
Mano224 <mano@example.com>
2024-08-05 03:13:27 -0700

    hh

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,20 @@ import uuid
 from datetime import datetime
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
-
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key =="__class__":
+                    continue
+                elif key =="created_at" or key =="updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                else:
+                    setattr(self, key, value)
 
     def save(self):
         """
